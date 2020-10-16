ccd9ca310c07dce697d9c14e80b284c513b7f8f4
isoppp <prog@d2deck.com>
2020-10-16 22:46:37 +0900

    show article content

--- src/pages/articles/[articleId]/index.tsx
+++ src/pages/articles/[articleId]/index.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router'
 import { NextPage } from 'next'
 import { useGetArticleQuery } from '@/generated/graphql'
+import Error from 'next/error'
 
 const ArticlePage: NextPage = () => {
   const router = useRouter()
@@ -10,13 +11,25 @@ const ArticlePage: NextPage = () => {
       id: articleId as string,
     },
   })
+
   if (loading) {
     return <p>...loading</p>
   }
   if (error) {
     return <p>{error.toString()}</p>
   }
-  return <div>{JSON.stringify(data)}</div>
+  if (!data?.articles_by_pk) {
+    return <Error statusCode={404} />
+  }
+
+  const { user, subject, content } = data.articles_by_pk
+  return (
+    <div className="max-w-screen-lg mx-auto p-10">
+      <div>{user.display_name}</div>
+      <div>{subject}</div>
+      <div>{content}</div>
+    </div>
+  )
 }
 
 export default ArticlePage
