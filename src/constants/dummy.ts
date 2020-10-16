bdc3a1fdfe6630d16268da3ff1df735f56d2e618
isoppp <prog@d2deck.com>
2020-10-17 00:24:33 +0900

    make it possible to add post

---
+++ src/constants/dummy.ts
@@ -0,0 +1,1 @@
+export const DUMMY_USER_ID = '85465b19-41eb-403e-babe-c372a9e12292'
--- src/pages/articles/new/index.tsx
+++ src/pages/articles/new/index.tsx
@@ -1,14 +1,30 @@
 import { useRouter } from 'next/router'
 import { NextPage } from 'next'
-import { useGetArticleQuery } from '@/generated/graphql'
+import { useGetArticleQuery, usePostArticleMutation } from '@/generated/graphql'
 import { useForm } from 'react-hook-form'
+import { DUMMY_USER_ID } from '@/constants/dummy'
 
 const ArticlesNew: NextPage = () => {
   const router = useRouter()
+  const [postArticle] = usePostArticleMutation()
   const { register, handleSubmit, watch, errors } = useForm()
-  const onSubmit = (data) => console.log(data)
-  const formClasses = 'w-full border border-gray-300 rounded-md p-4'
+  const onSubmit = async (formData) => {
+    const { data } = await postArticle({
+      variables: {
+        ...formData,
+        author_id: DUMMY_USER_ID,
+        published_at: 'now()',
+      },
+    })
+    if (data?.insert_articles_one) {
+      const aritcleId = data.insert_articles_one.id
+      router.push(`/articles/${aritcleId}`)
+    } else {
+      Promise.reject('POST unknown state')
+    }
+  }
 
+  const formClasses = 'w-full border border-gray-300 rounded-md p-4'
   return (
     <div>
       <form className="flex flex-col items-center gap-4" onSubmit={handleSubmit(onSubmit)}>
