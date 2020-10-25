627268b7ab11f129bc9f8d218086e73e5c43de89
isoppp <prog@d2deck.com>
2020-10-25 22:27:00 +0900

    add user.onCreate event in order to add custom claim to new users

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,30 @@
 import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+admin.initializeApp(functions.config().firebase)
+
+// https://github.com/hasura/graphql-engine/tree/master/community/sample-apps/firebase-jwt
+export const processSignUp = functions.auth.user().onCreate((user) => {
+  console.log(user)
+  const customClaims = {
+    'https://hasura.io/jwt/claims': {
+      'x-hasura-default-role': 'user',
+      'x-hasura-allowed-roles': ['user'],
+      'x-hasura-user-id': user.uid,
+    },
+  }
+  return admin
+    .auth()
+    .setCustomUserClaims(user.uid, customClaims)
+    .then(() => {
+      const metadataRef = admin.database().ref('metadata/' + user.uid)
+      return metadataRef.set({ refreshTime: new Date().getTime() })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+})
+
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info('Hello logs!', { structuredData: true })
   response.send('Hello from Firebase!')
